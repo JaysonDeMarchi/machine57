e9f5f47b6d97ed9d4f66dd64b32cf760c2b2054d
Jayson DeMarchi <jayson.demarchi@gmail.com>
2020-02-21 21:04:25 -0500

    Add content script to DOM
    Default to only pull in the LayeredNav

---
+++ src/contentScript/component.tsx
@@ -0,0 +1,7 @@
+import React, { FunctionComponent } from "react";
+import { LayeredNav } from "@src/components/layerednav";
+import "./styles.scss";
+
+export const ContentScript: FunctionComponent = () => {
+    return <LayeredNav />;
+};
---
+++ src/contentScript/index.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { ContentScript } from "./component";
+
+ReactDOM.render(<ContentScript />, document.getElementById("contentScript"));
