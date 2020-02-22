5fdf927c674a43b6eb2657616274485df493448a
Jayson DeMarchi <jayson.demarchi@gmail.com>
2020-02-22 00:39:54 -0500

    Insert LayeredNav into DOM

--- src/contentScript/index.tsx
+++ src/contentScript/index.tsx
@@ -2,4 +2,17 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { ContentScript } from "./component";
 
-ReactDOM.render(<ContentScript />, document.getElementById("contentScript"));
+const content = document.getElementsByClassName("content")[0];
+const productList = document.getElementsByClassName("products_list")[0];
+const productContent = document.createElement("div");
+productContent.id = "product_content";
+const layeredNav = document.createElement("div");
+layeredNav.id = "content_script";
+
+if (productList && content) {
+    content.insertBefore(productContent, productList);
+    productContent.appendChild(layeredNav);
+    productContent.appendChild(productList);
+}
+
+ReactDOM.render(<ContentScript />, document.getElementById("content_script"));
